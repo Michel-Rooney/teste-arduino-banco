d9acd65892a212f506cb481062bafbabc14099aa
Michel-Rooney <michelrooney16@gmail.com>
2023-02-13 22:52:36 -0300

    Deploy render

--- core/settings.py
+++ core/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
